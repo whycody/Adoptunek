9a149a4349a9aa343ab57789fd8f3fd9491ea972
Oktawian Kausz <play26400@gmail.com>
2019-10-09 20:01:11 +0200

    Configured primary and primary dark colors and added Glide library

---
+++ app/src/main/java/pl/adoptunek/adoptunek/splash/SplashActivity.kt
@@ -0,0 +1,17 @@
+package pl.adoptunek.adoptunek.splash
+
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.adoptunek.adoptunek.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
