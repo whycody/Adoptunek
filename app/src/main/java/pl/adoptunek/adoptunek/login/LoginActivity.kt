7e7e2f642b1be1da13d1c1ec4bf790a1ad21ed54
Oktawian Kausz <play26400@gmail.com>
2019-10-10 22:37:57 +0200

    Added TextWatcher to Register and Login activities

--- app/src/main/java/pl/adoptunek/adoptunek/login/LoginActivity.kt
+++ app/src/main/java/pl/adoptunek/adoptunek/login/LoginActivity.kt
@@ -2,17 +2,36 @@ package pl.adoptunek.adoptunek.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_register.emailText
+import kotlinx.android.synthetic.main.activity_register.passText
 import pl.adoptunek.adoptunek.R
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), TextWatcher {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        emailText.addTextChangedListener(this)
+        passText.addTextChangedListener(this)
+        nextBtn.isEnabled = false
     }
 
     override fun onDestroy() {
         super.onDestroy()
         overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_bottom)
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+        nextBtn.isEnabled = !(passText.text.toString().isEmpty()|| emailText.text.toString().isEmpty())
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
     }
 }
--- app/src/main/java/pl/adoptunek/adoptunek/register/RegisterActivity.kt
+++ app/src/main/java/pl/adoptunek/adoptunek/register/RegisterActivity.kt
@@ -2,17 +2,37 @@ package pl.adoptunek.adoptunek.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_login.emailText
+import kotlinx.android.synthetic.main.activity_login.passText
+import kotlinx.android.synthetic.main.activity_register.*
 import pl.adoptunek.adoptunek.R
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), TextWatcher {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        emailText.addTextChangedListener(this)
+        passText.addTextChangedListener(this)
+        repPassText.addTextChangedListener(this)
+        registerNextBtn.isEnabled = false
     }
 
     override fun onDestroy() {
         super.onDestroy()
         overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_bottom)
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+        registerNextBtn.isEnabled = !(passText.text.toString().isEmpty()||
+                emailText.text.toString().isEmpty()||repPassText.text.toString().isEmpty())
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
     }
 }
