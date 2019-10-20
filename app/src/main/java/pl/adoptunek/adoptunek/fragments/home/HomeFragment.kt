84caee76933610de453a9a7b4f1dc3499c2e9e38
Oktawian Kausz <play26400@gmail.com>
2019-10-20 21:51:35 +0200

    Finalized pet of week views

--- app/src/main/java/pl/adoptunek/adoptunek/fragments/home/HomeFragment.kt
+++ app/src/main/java/pl/adoptunek/adoptunek/fragments/home/HomeFragment.kt
@@ -1,23 +1,32 @@
 package pl.adoptunek.adoptunek.fragments.home
 
+import android.graphics.drawable.Drawable
+import android.media.Image
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import kotlinx.android.synthetic.main.fragment_home.view.*
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+import com.bumptech.glide.request.RequestOptions
 import pl.adoptunek.adoptunek.Pet
 import pl.adoptunek.adoptunek.Post
 
-import pl.adoptunek.adoptunek.R
 import pl.adoptunek.adoptunek.data.pet.PetContract
 import pl.adoptunek.adoptunek.data.pet.PetDaoImpl
 import pl.adoptunek.adoptunek.data.post.PostDaoImpl
 import pl.adoptunek.adoptunek.data.post.PostInterractor
 import pl.adoptunek.adoptunek.fragments.home.post.recycler.PostRecyclerAdapter
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import kotlinx.android.synthetic.main.fragment_home.view.*
+import pl.adoptunek.adoptunek.R
 
 class HomeFragment : Fragment(), PostInterractor, PetContract.PetInterractor {
 
@@ -26,14 +35,32 @@ class HomeFragment : Fragment(), PostInterractor, PetContract.PetInterractor {
     private lateinit var recycler: RecyclerView
     private val postDao = PostDaoImpl(this)
     private val petDao = PetDaoImpl(this)
+    private lateinit var firstPet: ImageView
+    private lateinit var secondPet: ImageView
+    private lateinit var thirdPet: ImageView
+    private lateinit var firstPetName: TextView
+    private lateinit var secondPetName: TextView
+    private lateinit var thirdPetName: TextView
+    private lateinit var petOfWeekView: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         recycler = view.postsRecycle
+        firstPet = view.findViewById(R.id.firstPet)
+        secondPet = view.findViewById(R.id.secondPet)
+        thirdPet = view.findViewById(R.id.thirdPet)
+        firstPetName = view.findViewById(R.id.firstPetName)
+        secondPetName = view.findViewById(R.id.secondPetName)
+        thirdPetName = view.findViewById(R.id.thirdPetName)
+        petOfWeekView = view.findViewById(R.id.petOfWeekView)
+        loadDrawableToImage(context!!.getDrawable(R.drawable.dog_of_week)!!, petOfWeekView)
+        loadDrawableToImage(context!!.getDrawable(R.drawable.dog_default)!!, firstPet)
+        loadDrawableToImage(context!!.getDrawable(R.drawable.dog_default)!!, secondPet)
+        loadDrawableToImage(context!!.getDrawable(R.drawable.dog_default)!!, thirdPet)
+        view.postsRecycle.layoutManager = LinearLayoutManager(activity)
         postDao.getPosts(10)
         petDao.getPetsOfWeek()
-        view.postsRecycle.layoutManager = LinearLayoutManager(activity)
         return view
     }
 
@@ -48,8 +75,35 @@ class HomeFragment : Fragment(), PostInterractor, PetContract.PetInterractor {
     }
 
     private fun loadPetsOfWeek(petlist: List<Pet>){
-        Glide.with(activity!!).load(petlist[0].profile_image).into(view!!.findViewById(R.id.firstPet))
-        Glide.with(activity!!).load(petlist[1].profile_image).into(view!!.findViewById(R.id.secondPet))
-        Glide.with(activity!!).load(petlist[2].profile_image).into(view!!.findViewById(R.id.thirdPet))
+        loadUriToImage(petlist[0].profile_image!!, firstPet)
+        loadUriToImage(petlist[1].profile_image!!, secondPet)
+        loadUriToImage(petlist[2].profile_image!!, thirdPet)
+        setupText(petlist)
+    }
+
+    private fun setupText(petlist: List<Pet>){
+        firstPetName.text = petlist[0].name
+        secondPetName.text = petlist[1].name
+        thirdPetName.text = petlist[2].name
+    }
+
+    private fun loadDrawableToImage(drawable: Drawable, view: ImageView){
+        var requestOptions = RequestOptions()
+        requestOptions = requestOptions.transform(CenterCrop(), RoundedCorners(16))
+        Glide.with(activity!!)
+            .load(drawable)
+            .apply(requestOptions)
+            .transition(withCrossFade())
+            .into(view)
+    }
+
+    private fun loadUriToImage(uri: Uri, view: ImageView){
+        var requestOptions = RequestOptions()
+        requestOptions = requestOptions.transform(CenterCrop(), RoundedCorners(16))
+        Glide.with(activity!!)
+            .load(uri)
+            .apply(requestOptions)
+            .transition(withCrossFade())
+            .into(view)
     }
 }
